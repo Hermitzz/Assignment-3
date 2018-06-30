310b5923d697840ed35587056acb45365c2d6ac5
chexy112 <40650701+chexy112@users.noreply.github.com>
2018-07-01 05:47:07 +0930

    fixed

--- ContractorSoftware/ContractorSoftware.cs
+++ ContractorSoftware/ContractorSoftware.cs
@@ -13,106 +13,106 @@ using System.Windows.Forms;
 
 namespace ContractorSoftware
 {
-    public partial class ContractorSoftware : Form
-    {
+	public partial class ContractorSoftware : Form
+	{
 		// variables for sql connection
-        private SqlConnection conn;
-        private SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
-        private BindingSource bindingSource1 = new BindingSource();
-        private SqlDataAdapter dataAdapter = new SqlDataAdapter();
+		private SqlConnection conn;
+		private SqlConnectionStringBuilder csb = new SqlConnectionStringBuilder();
+		private BindingSource bindingSource1 = new BindingSource();
+		private SqlDataAdapter dataAdapter = new SqlDataAdapter();
 
 		// keeps track of the currently selected row in the DataGridView using cell click
 		DataGridViewRow CurrentlySelectedRow;
 
 		public ContractorSoftware()
-        {
-            InitializeComponent();
+		{
+			InitializeComponent();
 			// setting connection string
-            string filePath = Path.Combine(System.IO.Path.GetFullPath(@"..\..\"), "ContractorDataBase.mdf");
-            string connection = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = " +
-                                filePath + @"; Integrated Security = True";
-            csb.ConnectionString = connection;
-        }
+			string filePath = Path.Combine(System.IO.Path.GetFullPath(@"..\..\"), "ContractorDataBase.mdf");
+			string connection = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = " +
+								filePath + @"; Integrated Security = True";
+			csb.ConnectionString = connection;
+		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			// setting the datasource for the DataGridView to the jobs table in the database
-            DataGridView.DataSource = bindingSource1;
-            GetData("SELECT * from Jobs");
+			DataGridView.DataSource = bindingSource1;
+			GetData("SELECT * from Jobs");
 
-        }
+		}
 
-        private void GetData(string selectCommand)
-        {
-            try
-            {
-                // Specify a connection string. Replace the given value with a 
-                // valid connection string for a Northwind SQL Server sample
-                // database accessible to your system.
-                String connectionString = csb.ConnectionString;
+		private void GetData(string selectCommand)
+		{
+			try
+			{
+				// Specify a connection string. Replace the given value with a 
+				// valid connection string for a Northwind SQL Server sample
+				// database accessible to your system.
+				String connectionString = csb.ConnectionString;
 
-                // Create a new data adapter based on the specified query.
-                dataAdapter = new SqlDataAdapter(selectCommand, connectionString);
+				// Create a new data adapter based on the specified query.
+				dataAdapter = new SqlDataAdapter(selectCommand, connectionString);
 
-                // Create a command builder to generate SQL update, insert, and
-                // delete commands based on selectCommand. These are used to
-                // update the database.
-                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+				// Create a command builder to generate SQL update, insert, and
+				// delete commands based on selectCommand. These are used to
+				// update the database.
+				SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
 
-                // Populate a new data table and bind it to the BindingSource.
-                DataTable table = new DataTable();
-                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
-                dataAdapter.Fill(table);
-                bindingSource1.DataSource = table;
+				// Populate a new data table and bind it to the BindingSource.
+				DataTable table = new DataTable();
+				table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+				dataAdapter.Fill(table);
+				bindingSource1.DataSource = table;
 
-            }
-            catch (SqlException)
-            {
-                MessageBox.Show("SqlException");
-            }
-        }
+			}
+			catch (SqlException)
+			{
+				MessageBox.Show("SqlException");
+			}
+		}
 
 		// opens a new form holding the required invoice information which uses graphics to print a copy of said new form
-        private void PrintButton_Click(object sender, EventArgs e)
+		private void PrintButton_Click(object sender, EventArgs e)
 		{
-            PrintForm printForm = new PrintForm(JobShortDescriptionTextBox.Text, ClientNameTextBox.Text, ClientAddressTextBox.Text,
-                                                ClientLandLineTextBox.Text, ClientMobilePhoneTextBox.Text, ClientBusinessNameTextBox.Text,
-                                                ClientEmailTextBox.Text, AmountTextBox.Text);
-            printForm.Show();
-        }
+			PrintForm printForm = new PrintForm(JobShortDescriptionTextBox.Text, ClientNameTextBox.Text, ClientAddressTextBox.Text,
+												ClientLandLineTextBox.Text, ClientMobilePhoneTextBox.Text, ClientBusinessNameTextBox.Text,
+												ClientEmailTextBox.Text, AmountTextBox.Text);
+			printForm.Show();
+		}
 
 		// sets the CurrentlySelectedRow and fills the form fields with the data from the selected row.
-        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex != DataGridView.TopLeftHeaderCell.RowIndex)
-            {
+		private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+		{
+			if (e.RowIndex != DataGridView.TopLeftHeaderCell.RowIndex)
+			{
 
-                CurrentlySelectedRow = DataGridView.Rows[e.RowIndex];
+				CurrentlySelectedRow = DataGridView.Rows[e.RowIndex];
 
-                JobShortDescriptionTextBox.Text = CurrentlySelectedRow.Cells[1].Value.ToString();
-                JobLocationTextBox.Text = CurrentlySelectedRow.Cells[2].Value.ToString();
-                JobDateTimePicker.Value = (DateTime)CurrentlySelectedRow.Cells[3].Value;
-                JobPriorityTextBox.Text = CurrentlySelectedRow.Cells[4].Value.ToString();
+				JobShortDescriptionTextBox.Text = CurrentlySelectedRow.Cells[1].Value.ToString();
+				JobLocationTextBox.Text = CurrentlySelectedRow.Cells[2].Value.ToString();
+				JobDateTimePicker.Value = (DateTime)CurrentlySelectedRow.Cells[3].Value;
+				JobPriorityTextBox.Text = CurrentlySelectedRow.Cells[4].Value.ToString();
 
-                String currentClientId = CurrentlySelectedRow.Cells[5].Value.ToString();
-                //TODO 
-                //use for loop to find the current client and show the client details
-                
+				String currentClientId = CurrentlySelectedRow.Cells[5].Value.ToString();
+				//TODO 
+				//use for loop to find the current client and show the client details
+				
 
-                String currentContractorId = CurrentlySelectedRow.Cells[6].Value.ToString();
-                //TODO
-                //user for loop to find the current contractor and show the contractor name in the print form
+				String currentContractorId = CurrentlySelectedRow.Cells[6].Value.ToString();
+				//TODO
+				//user for loop to find the current contractor and show the contractor name in the print form
 
-                if (CurrentlySelectedRow.Cells[7].Value.ToString() == "0")
-                {
-                    CompletionCheckBox.Checked = false;
-                }
-                else
-                {
-                    CompletionCheckBox.Checked = true;
-                }
-            }
-        }
+				if (CurrentlySelectedRow.Cells[7].Value.ToString() == "0")
+				{
+					CompletionCheckBox.Checked = false;
+				}
+				else
+				{
+					CompletionCheckBox.Checked = true;
+				}
+			}
+		}
 
 		// below are tooltips to increase software usability
 		private void ImportButton_MouseHover(object sender, EventArgs e)
